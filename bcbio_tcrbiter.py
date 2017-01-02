d0cf08a8d49fd41ceb866b3c9f8453277469dff3
Rory Kirchner <rory.kirchner@gmail.com>
2017-01-01 19:20:17 -0500

    Add script to run tcrBiter on bcbio output easily.
    Takes a path to the bcbio YAML file for the run and
    a BLASTdb and runs tcrbiter.

---
+++ bcbio_tcrbiter.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+import yaml
+from subprocess import check_call
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("yaml", 
+        help="Path to bcbio-nextgen sample configuration YAML file.")
+    parser.add_argument("blastdb",
+        help="Path to BLAST nucleotide database.")
+    args = parser.parse_args()
+    blastdb = args.blastdb
+    base_cmd = ("python tcrBiter_betav2.py --r1 {r1} --r2 {r2} "
+                "--blastdb {blastdb}")
+    with open(args.yaml) as in_handle:
+        summary = yaml.load(in_handle)
+    details =  summary["details"]
+    files = [x["files"] for x in details]
+    r1files = [x[0] for x in files]
+    r2files = [x[1] for x in files]
+    names = [x["description"] for x in details]
+    for r1, r2, name in zip(r1files, r2files, names):
+        print "Processing %s." % name
+        print base_cmd.format(**locals())
+        check_call(base_cmd.format(**locals()), shell=True)
